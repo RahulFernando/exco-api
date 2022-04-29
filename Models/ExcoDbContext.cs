fba56fc2d2c7fbb1ddc89ced8a0ebb1a6a27bef1
RahulFernando <rahulrulz680@gmail.com>
2022-04-29 21:17:01 +0530

    feat: lending api

--- Models/ExcoDbContext.cs
+++ Models/ExcoDbContext.cs
@@ -10,5 +10,24 @@ namespace exco_api.Models
         }
 
         public DbSet<Lending> Lendings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Lending>().HasData(
+                new Lending
+                {
+                    id = 1,
+                    name = "1984 by george orwell",
+                    img = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327144697l/3744438.jpg"
+                },
+
+                new Lending
+                {
+                    id = 2,
+                    name = "The Lord of the Rings by J.R.R. Tolkien",
+                    img = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1566425108l/33.jpg"
+                }
+            );
+        }
     }
 }
